5e6e3657e543a7e3814cdb2dc6fb34c56dc5175d
gcalquin <gcalquin@gmail.com>
2021-09-19 20:05:18 -0300

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,78 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: %i[ show edit update destroy ]
+  # GET /posts or /posts.json
+  def index
+
+    if params[:search]
+      @posts = Post.where('title LIKE ?', "%#{params[:search]}%").or(Post.where('content LIKE ?', "%#{params[:search]}%"))
+      
+      respond_to do |format|
+        format.js
+      end
+    else
+      @posts = Post.all
+    end
+
+  end 
+
+  # GET /posts/1 or /posts/1.json
+  def show
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # GET /posts/1/edit
+  def edit
+  end
+
+  # POST /posts or /posts.json
+  def create
+    @posts = Post.all
+    @post = Post.new(post_params)
+    respond_to do |format|
+        if @post.save
+          format.js {render nothing: true} 
+        end
+    end
+  end
+
+  # PATCH/PUT /posts/1 or /posts/1.json
+  def update
+    @posts = Post.all.order(updated_at: :desc)
+    respond_to do |format|
+      if @post.update(post_params)
+        format.js {render nothing: true} 
+        format.html { redirect_to @post, notice: "Post was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /posts/1 or /posts/1.json
+  def destroy
+    if @post.destroy
+      @posts = Post.all.order(updated_at: :desc)
+      respond_to do |format|
+        format.js { render nothing: true }
+        format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+
+end
